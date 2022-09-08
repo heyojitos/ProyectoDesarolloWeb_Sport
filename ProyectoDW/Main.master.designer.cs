a0c01ae8b4231c93ac22c74a6f01c65aff95fb7e
Marvin Alonso <malonsoa@miumg.edu.gt>
2022-09-07 21:32:10 -0600

    Creacion de la pagina para mantenimiento de Clientes WebCliente.aspx

--- ProyectoDW/Main.master.designer.cs
+++ ProyectoDW/Main.master.designer.cs
@@ -8,36 +8,9 @@
 //------------------------------------------------------------------------------
 
 namespace ProyectoDW {
-
-
+    
+    
     public partial class MainMaster {
-        
-        /// <summary>
-        /// ASPxNavBar1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxNavBar ASPxNavBar1;
-        
-        /// <summary>
-        /// XmlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSource1;
-        
-        /// <summary>
-        /// ASPxMenu2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxMenu ASPxMenu2;
         
         /// <summary>
         /// MainContent control.
@@ -49,40 +22,12 @@ namespace ProyectoDW {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
         
         /// <summary>
-        /// LeftPane control.
+        /// XmlDataSourceLeft control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::DevExpress.Web.ASPxPanel LeftPane;
-
-        /// <summary>
-        /// RightPane control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxPanel RightPane;
-        
-        /// <summary>
-        /// ASPxCloudControl1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.Web.ASPxCloudControl ASPxCloudControl1;
-
-        
-        /// <summary>
-        /// XmlDataSource3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSource3;
+        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSourceLeft;
     }
 }
