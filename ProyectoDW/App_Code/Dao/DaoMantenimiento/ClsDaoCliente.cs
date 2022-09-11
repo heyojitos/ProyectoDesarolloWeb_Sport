05bb80ddeffba0e75d7cddf9557ec5cc9dd4a146
Marvin Antonio <malonsoa@miumg.edu.gt>
2022-09-11 17:26:59 -0600

    Auto stash before merge of "MARVIN" and "origin/master"

--- ProyectoDW/App_Code/Dao/DaoMantenimiento/ClsDaoCliente.cs
+++ ProyectoDW/App_Code/Dao/DaoMantenimiento/ClsDaoCliente.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using DXWebApplication.App_Code.Utilidades;
+using ProyectoDW.App_Code.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ProyectoDW.App_Code.Dao.DaoMantenimiento
 {
-    public class ClsDaoCliente
+    public class ClsDaoCliente : ClsDataLayer
     {
+        ClsUsuario objUsuario = new ClsUsuario();
+        ClsErrorHandler log = new ClsErrorHandler();
+        String strSql = string.Empty;
+
+        public bool getClienteAll() {
+
+            try
+            {
+                strSql = "SELECT ID_USUARIO, NOMBRE, APELLIDO, EMAIL, PASSWORD FROM TB_USUARIO";
+
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                //throw;
+            }
+            return true;
+        }
     }
 }
