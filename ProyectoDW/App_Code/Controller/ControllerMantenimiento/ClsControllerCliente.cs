18a4679361d01f1c99d96e88b973a441518d065c
Marvin Alonso <malonsoa@miumg.edu.gt>
2022-09-11 18:24:53 -0600

    Creacion de Clases Dao y Controller para manejo de usuarios

--- ProyectoDW/App_Code/Controller/ControllerMantenimiento/ClsControllerCliente.cs
+++ ProyectoDW/App_Code/Controller/ControllerMantenimiento/ClsControllerCliente.cs
@@ -1,11 +1,86 @@
-﻿using System;
+﻿using DXWebApplication.App_Code.Utilidades;
+using ProyectoDW.App_Code.Dao;
+using ProyectoDW.App_Code.Dao.DaoMantenimiento;
+using ProyectoDW.App_Code.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ProyectoDW.App_Code.Controller.ControllerMantenimiento
 {
-    public class ClsControllerCliente
+    public class ClsControllerCliente : ClsController
     {
+        ClsErrorHandler log = new ClsErrorHandler();
+        ClsDaoCliente objCliente = new ClsDaoCliente();
+
+        public bool GetClienteAll()
+        {
+            try
+            {
+                if (objCliente.getClienteAll())
+                {
+                    DsReturn = objCliente.DsReturn;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                //throw;
+            }
+            return false;
+        }
+
+        public bool InsertCliente(ClsUsuario usuario)
+        {
+            try
+            {
+                if (objCliente.InsertCliente(usuario))
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+            return false;
+        }
+
+        public bool UpdateCliente(ClsUsuario usuario)
+        {
+            try
+            {
+                if (objCliente.UpdateCliente(usuario))
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+            return false;
+        }
+
+        public bool DeleteCliente(ClsUsuario usuario)
+        {
+            try
+            {
+                if (objCliente.DeleteCliente(usuario))
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+            return false;
+        }
     }
 }
--- ProyectoDW/App_Code/Dao/DaoMantenimiento/ClsDaoCliente.cs
+++ ProyectoDW/App_Code/Dao/DaoMantenimiento/ClsDaoCliente.cs
@@ -1,4 +1,6 @@
-﻿using DXWebApplication.App_Code.Utilidades;
+﻿using DXWebApplication.App_Code.Dal;
+
+using DXWebApplication.App_Code.Utilidades;
 using ProyectoDW.App_Code.Models;
 using System;
 using System.Collections.Generic;
@@ -9,16 +11,29 @@ namespace ProyectoDW.App_Code.Dao.DaoMantenimiento
 {
     public class ClsDaoCliente : ClsDataLayer
     {
-        ClsUsuario objUsuario = new ClsUsuario();
+        ClsConexion objSql = new ClsConexion();
         ClsErrorHandler log = new ClsErrorHandler();
         String strSql = string.Empty;
+
+        public bool ExecuteSql(string strSql)
+        {
+            try
+            {
+                return objSql.ejecutarNonQuery(strSql);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+        }
 
         public bool getClienteAll() {
 
             try
             {
                 strSql = "SELECT ID_USUARIO, NOMBRE, APELLIDO, EMAIL, PASSWORD FROM TB_USUARIO";
-
+                DsReturn = objSql.EjectuaSQL(strSql, "Usuario");
             }
             catch (Exception ex)
             {
@@ -26,6 +41,24 @@ namespace ProyectoDW.App_Code.Dao.DaoMantenimiento
                 //throw;
             }
             return true;
+        }
+
+        public bool InsertCliente(ClsUsuario usuario)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+
+        public bool UpdateCliente(ClsUsuario usuario)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+
+        public bool DeleteCliente(ClsUsuario usuario)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
         }
     }
 }
