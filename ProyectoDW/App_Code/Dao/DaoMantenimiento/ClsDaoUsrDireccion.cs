17cf1d00384c794cd0e88520cc0a59f35d3646f7
Marvin Antonio <malonsoa@miumg.edu.gt>
2022-09-11 19:51:01 -0600

    Creacion clases dao y controller
    Creación clases dao y controller para telefonos y direcciones  del usuario

---
+++ ProyectoDW/App_Code/Dao/DaoMantenimiento/ClsDaoUsrDireccion.cs
@@ -0,0 +1,63 @@
+﻿using DXWebApplication.App_Code.Dal;
+using DXWebApplication.App_Code.Utilidades;
+using ProyectoDW.App_Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDW.App_Code.Dao.DaoMantenimiento
+{
+    public class ClsDaoUsrDireccion : ClsDataLayer
+    {
+        ClsConexion objSql = new ClsConexion();
+        ClsErrorHandler log = new ClsErrorHandler();
+        string strSql = string.Empty;
+
+        public bool getDireccionAll()
+        {
+            try
+            {
+                strSql = "";
+                DsReturn = objSql.EjectuaSQL(strSql, "Direccion");
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+            return true;
+        }
+
+        public bool ExecuteSql(string strSql)
+        {
+            try
+            {
+                return objSql.ejecutarNonQuery(strSql);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+        }
+
+        public bool InsertDireccion(ClsUsrDireccion direccion)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+
+        public bool UpdateDireccion(ClsUsrDireccion direccion)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+
+        public bool DeleteDireccion(ClsUsrDireccion direccion)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+    }
+}
---
+++ ProyectoDW/App_Code/Dao/DaoMantenimiento/ClsDaoUsrTelefono.cs
@@ -0,0 +1,63 @@
+﻿using DXWebApplication.App_Code.Dal;
+using DXWebApplication.App_Code.Utilidades;
+using ProyectoDW.App_Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDW.App_Code.Dao.DaoMantenimiento
+{
+    public class ClsDaoUsrTelefono : ClsDataLayer
+    {
+        ClsConexion objSql = new ClsConexion();
+        ClsErrorHandler log = new ClsErrorHandler();
+        string strSql = string.Empty;
+
+        public bool GetTelefonoAll()
+        {
+            try
+            {
+                strSql = "";
+                DsReturn = objSql.EjectuaSQL(strSql, "Telefono");
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+            return true;
+        }
+
+        public bool ExecuteSql(string strSql)
+        {
+            try
+            {
+                return objSql.ejecutarNonQuery(strSql);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString(), ex.StackTrace);
+                return false;
+            }
+        }
+
+        public bool InsertTelefono(ClsUsrTelefono telefono)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+
+        public bool UpdateTelefono(ClsUsrTelefono telefono)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+
+        public bool DeleteTelefono(ClsUsrTelefono telefono)
+        {
+            strSql = "";
+            return ExecuteSql(strSql);
+        }
+    }
+}
