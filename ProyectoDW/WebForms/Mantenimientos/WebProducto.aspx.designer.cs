7f115a61d5b4c4f89d00a8e411f0cfb8384bb828
Marvin Antonio <malonsoa@miumg.edu.gt>
2022-09-19 21:57:05 -0600

    Pruebas de imagen producto
    prueba del ingreso de imagen de un producto

--- ProyectoDW/WebForms/Mantenimientos/WebProducto.aspx.designer.cs
+++ ProyectoDW/WebForms/Mantenimientos/WebProducto.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace ProyectoDW.WebForms.Mantenimientos {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource sqlDtsCategoria;
+        
+        /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }
