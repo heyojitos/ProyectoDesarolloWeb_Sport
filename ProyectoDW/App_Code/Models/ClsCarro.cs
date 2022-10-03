885240035b7cf1fdf3a2037b6bcc629330fad9a2
Javier Osorio <josorioe@miumg.edu.gt>
2022-10-02 18:13:15 -0600

    Clases de carrito

---
+++ ProyectoDW/App_Code/Models/ClsCarro.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDW.App_Code.Models
+{
+    public class ClsCarro
+    {
+        private int id_regitro;
+        private string codigo_producto;
+        private string descripcion_producto;
+        private int cantidad;
+        private decimal subtotal;
+        private decimal total;
+
+        public int Id_regitro
+        {
+            get
+            {
+                return id_regitro;
+            }
+
+            set
+            {
+                id_regitro = value;
+            }
+        }
+
+        public string Codigo_producto
+        {
+            get
+            {
+                return codigo_producto;
+            }
+
+            set
+            {
+                codigo_producto = value;
+            }
+        }
+
+        public string Descripcion_producto
+        {
+            get
+            {
+                return descripcion_producto;
+            }
+
+            set
+            {
+                descripcion_producto = value;
+            }
+        }
+
+        public int Cantidad
+        {
+            get
+            {
+                return cantidad;
+            }
+
+            set
+            {
+                cantidad = value;
+            }
+        }
+
+        public decimal Subtotal
+        {
+            get
+            {
+                return subtotal;
+            }
+
+            set
+            {
+                subtotal = value;
+            }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                return total;
+            }
+
+            set
+            {
+                total = value;
+            }
+        }
+    }
+}
