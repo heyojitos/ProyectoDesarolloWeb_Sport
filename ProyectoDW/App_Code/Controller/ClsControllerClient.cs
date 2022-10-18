9e15f8c9a53863e37e6ac0f90c6281afa99cf635
Antonio <malonsoa@miumg.edu.gt>
2022-10-17 23:58:52 -0600

    Creacion clase para login

---
+++ ProyectoDW/App_Code/Controller/ClsControllerClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDW.App_Code.Controller
+{
+    public class ClsControllerClient
+    {
+    }
+}
---
+++ ProyectoDW/App_Code/Models/ClsClient.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoDW.App_Code.Models
+{
+    
+    public class ClsClient
+    {
+        private int idCliente;
+        private String nombre;
+        private String apellido;
+        private String correo;
+        private String clave;
+        private Boolean reestablecer;
+        private String fechaReg;
+
+        public int IdCliente
+        {
+            get
+            {
+                return idCliente;
+            }
+
+            set
+            {
+                idCliente = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public string Correo
+        {
+            get
+            {
+                return correo;
+            }
+
+            set
+            {
+                correo = value;
+            }
+        }
+
+        public string Clave
+        {
+            get
+            {
+                return clave;
+            }
+
+            set
+            {
+                clave = value;
+            }
+        }
+
+        public bool Reestablecer
+        {
+            get
+            {
+                return reestablecer;
+            }
+
+            set
+            {
+                reestablecer = value;
+            }
+        }
+
+        public string FechaReg
+        {
+            get
+            {
+                return fechaReg;
+            }
+
+            set
+            {
+                fechaReg = value;
+            }
+        }
+    }
+}
